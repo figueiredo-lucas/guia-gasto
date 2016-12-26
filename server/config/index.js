40d8d5b21dde1ba9de55aad6c1016c17d5bb826e
figueiredo.lucas <ccomp.lucas@gmail.com>
2016-12-26 15:04:49 -0200

    Correção para openshift

--- server/config/index.js
+++ server/config/index.js
@@ -4,9 +4,16 @@ var path = require('path');
 
 module.exports = {
     mongo: {
-        uri: 'mongodb://localhost/guia-gasto'
+        uri: (function() {
+            var db_name = 'guia-gasto';
+            var mongo_connection = 'mongodb://localhost/';
+            if (process.env.OPENSHIFT_MONGODB_DB_URL) {
+                mongo_connection = process.env.OPENSHIFT_MONGODB_DB_URL;
+            }
+            return mongo_connection + db_name;
+        })()
     },
-    port: 3000,
-    ip: '0.0.0.0',
+    port: process.env.OPENSHIFT_NODEJS_PORT || 3000,
+    ip: process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     root: path.normalize(__dirname + '/../..')
 };
