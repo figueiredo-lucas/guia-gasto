2442b022d8c92e4f2aafd099feab94ef285398b9
figueiredo.lucas <ccomp.lucas@gmail.com>
2016-12-27 09:28:11 -0200

    Testes para mongo

--- server/config/index.js
+++ server/config/index.js
@@ -5,11 +5,16 @@ var path = require('path');
 module.exports = {
     mongo: {
         uri: (function() {
-            var mongo_connection = process.env.MONGOLAB_URI ||
-                process.env.MONGOHQ_URL ||
-                process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME ||
-                'mongodb://localhost/guia-gasto';
-            console.log(mongo_connection);
+            var mongo_connection = 'mongodb://localhost/guia-gasto';
+            console.log(process.env);
+            if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+                mongo_connection = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+                    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+                    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+                    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+                    process.env.OPENSHIFT_APP_NAME;
+            }
+            console.log(mongo_connection)
             return mongo_connection;
         })()
     },
